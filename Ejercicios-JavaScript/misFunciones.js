72277625669c6802254eaf66530e1528e7c7f7aa
tomas <1600351@ucc.edu.ar>
2019-05-22 09:45:28 -0300

    javascript

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -9,3 +9,40 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function saludar() {
+
+    alert("NISAAAAAAAAAAAAAAA");
+
+}
+
+function convertir(id_elem, valor_elem) {
+
+    if (isNaN(valor_elem)){
+        alert("El valor de "+id_elem+" no es un elemento")
+        document.getElementById(id_elem).value = "";
+    }
+
+    if(!isNaN(valor_elem)){
+    if(id_elem =='metro'){
+        document.getElementById("pulgada").value = valor_elem * 39.3701;
+        document.getElementById("pie").value = valor_elem * 3.28084;
+        document.getElementById("yarda").value = valor_elem * 1.09361;
+    }
+    if (id_elem == 'yarda'){
+        document.getElementById("pulgada").value = valor_elem * 36;
+        document.getElementById("pie").value = valor_elem * 3;
+        document.getElementById("metro").value = valor_elem * 0.9144;
+    }
+    if (id_elem == 'pulgada'){
+        document.getElementById("metro").value = valor_elem * 0.0254;
+        document.getElementById("pie").value = valor_elem * 0.08333;
+        document.getElementById("yarda").value = valor_elem * 0.0277778;
+    }
+    if (id_elem == 'pie'){
+        document.getElementById("metro").value = valor_elem * 0.3048;
+        document.getElementById("pulgada").value = valor_elem * 12;
+        document.getElementById("yarda").value = valor_elem * 0.333333;
+    }
+
+}}
